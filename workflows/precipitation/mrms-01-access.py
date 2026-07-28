7538f2c727282c3857723346f6e50df15e3ce585
MeghanTHayden <meghan.t.hayden@nasa.gov>
2026-07-28 10:09:45 -0600

    add option to access icechunk store without dynamical

--- workflows/precipitation/mrms-01-access.py
+++ workflows/precipitation/mrms-01-access.py
@@ -50,14 +50,30 @@ warnings.filterwarnings(
 # Access: S3 bucket (Alternative: HTTPS endpoint at "https://mrms.ncep.noaa.gov/")
 # Notes:
 # - see examples for working with MRMS data: https://projectpythia.org/mrms-cookbook/
-# - virtualization is not an option directly from NOAA because of .gzip format, must unzip in local S3 first...
+# - virtualization is not an option directly from NOAA because of .gzip format, must unzip in local S3 first
 # - update (7/10/26): there is an existing icechunk store by dynamical.org (https://dynamical.org/catalog/noaa-mrms-conus-analysis-hourly/)
+# - update (7/28/26): the existing store can be accessed with icechunk functionality, without using dynamical code
 
 # Call existing icechunk store through dynamical catalog
 
-ds = dynamical_catalog.open("noaa-mrms-conus-analysis-hourly", chunks=None)
+#ds = dynamical_catalog.open("noaa-mrms-conus-analysis-hourly", chunks=None)
 
-# Select location and time range for data access
+# Alternatively, call using standard icechunk functionality
+# url = "s3://dynamical-noaa-mrms/noaa-mrms-conus-analysis-hourly/v0.3.0.icechunk/"
+
+storage = icechunk.s3_storage(
+    bucket="dynamical-noaa-mrms",                        
+    prefix="noaa-mrms-conus-analysis-hourly/v0.3.0.icechunk/",    
+    anonymous=True                                
+)
+
+repo = icechunk.Repository.open(storage)
+session = repo.readonly_session(branch="main") # Check out the main branch or a specific commit
+
+ds = xr.open_zarr(session.store)
+
+
+# Access data for a specific location and time step
 subset_range = ds["precipitation_surface"].sel(
     latitude=40, 
     longitude=-90, 
